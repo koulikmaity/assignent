a19d2bfbbe373833fd685e49858a27288b359ff1
Koulik Maity <maitykoulik2003@mail.com>
2024-10-13 00:04:54 +0530

    Initial Commit

---
+++ assignent/assignentApp.swift
@@ -0,0 +1,17 @@
+//
+//  assignentApp.swift
+//  assignent
+//
+//  Created by Koulik Maity on 13/10/24.
+//
+
+import SwiftUI
+
+@main
+struct assignentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ assignentUITests/assignentUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  assignentUITestsLaunchTests.swift
+//  assignentUITests
+//
+//  Created by Koulik Maity on 13/10/24.
+//
+
+import XCTest
+
+final class assignentUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
